f84740b4463d44e4f24485ddc15bae4bec70c2df
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-09-13 15:51:15 -0300

    setup files created for installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# NOTE: The configuration for the package, including the name, version, and
+# other information are set in the setup.cfg file.
+
+import os
+import sys
+
+from setuptools import setup
+
+setup()
