737084d497670ca64e842619bb1bef52e622a393
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-09-13 22:53:13 -0300

    fix load of YAML. need to set Loader when calling yaml.load()

--- binrates/io/utils.py
+++ binrates/io/utils.py
@@ -1,8 +1,21 @@
+"""Module to load YAML configuration files"""
+
 import logging
+
+import yaml
 
 logger = logging.getLogger(__name__)
 
 
 def load_config(fname):
+    """Load configuration file with YAML format
 
-    return None
+    Parameters
+    ----------
+    fname
+
+    Returns
+    -------
+    """
+    with open(fname, "r") as f:
+        return yaml.load(f, Loader=yaml.FullLoader)
