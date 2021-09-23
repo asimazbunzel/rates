b56920b8e9fe0756253138317a81de80b6a7f44e
Adolfo Simaz Bunzel <asimazbunzel@gmail.com>
2021-09-22 21:11:33 -0300

    load TargetRegion into main iteration

--- binrates/base.py
+++ binrates/base.py
@@ -9,13 +9,13 @@ import logging
 
 from binrates.io.logger import set_logging
 from binrates.io.utils import load_config
-from binrates.population import Population
+from binrates.population import Population, TargetRegion
 
 
 logger = logging.getLogger(__name__)
 
 
-def init_binaries(BinPopConfig):
+def init_random_binaries(BinPopConfig):
     """Function to initialize binary populations"""
 
     msg = "instantiating Population object for random population from "
@@ -26,9 +26,28 @@ def init_binaries(BinPopConfig):
     number = BinPopConfig["number"]
     seed = BinPopConfig["seed"]
 
+    # define the random binary pop.
     BinPop = Population(number=number, seed=seed, Config=BinPopConfig)
 
     return BinPop
+
+
+def load_target_region(TargetRegionConfig):
+    """Function that loads a target region to which rates will be evaluated"""
+
+    # get some stuff
+    load_from_file = TargetRegionConfig["load_from_file"]
+    fname = ""
+    if load_from_file:
+        fname = TargetRegionConfig["fname"]
+    make_test = TargetRegionConfig["make_test_with_figure"]
+    shape = ""
+    if make_test:
+        shape = TargetRegionConfig["shape_of_figure"]
+
+    Region = TargetRegion(load_from_file, fname, make_test, shape)
+
+    return Region
 
 
 def eval_rates(config_fname: str = "", log_level: str = None, **kwargs):
@@ -53,8 +72,13 @@ def eval_rates(config_fname: str = "", log_level: str = None, **kwargs):
         )
 
     # create binary population
-    logger.info("initializing binary populations")
-    BinPop = init_binaries(Config["BinaryPopulation"])
+    logger.info("initializing random binary population")
+    BinPop = init_random_binaries(Config["BinaryPopulation"])
     logger.info(f"Population succesfully computed in `{BinPop}`")
+
+    # now get region of binary parameter space to which rates will be eval
+    logger.info("loading target region of binary parameter space")
+    BinPop = load_target_region(Config["TargetRegion"])
+    logger.info("target region succesfully loaded")
 
     return None
